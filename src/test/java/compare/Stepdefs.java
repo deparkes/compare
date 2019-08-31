24a012cfcf81287aa673ba91a4037e8ab7be602d
Duncan Parkes <deparkes@gmail.com>
2019-08-31 17:38:31 +0100

    Update feature tests

--- src/test/java/compare/Stepdefs.java
+++ src/test/java/compare/Stepdefs.java
@@ -10,14 +10,20 @@ public class Stepdefs {
     private double threshold;
     private boolean actualAnswer;
 
-    @Given("^I do not provide a threshold$")
+    @Given("I want to make a comparison")
     public void no_threshold() {
     }
 
-    @When("^When I provide \"([^\"]*)\" and \"([^\"]*)\"$")
+    @When("When I provide \"([^\"]*)\" and \"([^\"]*)\"")
     public void i_ask_whether_it_s_Friday_yet(String string, String string2) {
         Compare compare = new Compare();
         actualAnswer = compare.compare(string, string2);
+    }
+
+    @When("When I provide {string} and {string} with threshold {float}")
+    public void i_ask_whether_it_s_Friday_yet(String string, String string2, Double threhsold) {
+        Compare compare = new Compare();
+        actualAnswer = compare.compare(string, string2, threshold);
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
--- src/test/resources/compare/test_compare.feature
+++ src/test/resources/compare/test_compare.feature
@@ -2,11 +2,21 @@ Feature: Are these words the same
   Sometimes it is useful to know
 
   Scenario: Exactly the same word
-    Given I do not provide a threshold
+    Given I want to make a comparison
     When When I provide "word1" and "word1"
     Then I should be told "true"
 
   Scenario: Totally different word
-    Given I do not provide a threshold
+    Given I want to make a comparison
     When When I provide "x" and "word1"
+    Then I should be told "false"
+
+  Scenario: Similar enough word
+    Given I want to make a comparison
+    When When I provide "word1" and "word2" with threshold 0.1
+    Then I should be told "true"
+
+  Scenario: Not similar enough word
+    Given I want to make a comparison
+    When When I provide "word1" and "word2" with threshold 0.9
     Then I should be told "false"
